e10e9c39132513bdff8e59f873a383a103d19f92
Jonathan <j@miste.io>
2019-12-17 15:12:54 +0100

    Add version 1.18.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sdk-ermeo',
-    version='1.18.3',
+    version='1.18.4',
     description='Ermeo SDK for interacting with the API',
     url='https://github.com/ermeo/sdk-python',
     author='Ermeo',
